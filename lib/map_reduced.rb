31c579b2bb7584159504f263f789e825dd4dad84
Scott Burton <scottburton11@gmail.com>
2010-09-05 20:11:24 -0700

    require active_support inflector module along with all of core_ext

--- lib/map_reduced.rb
+++ lib/map_reduced.rb
@@ -1,10 +1,11 @@
 require 'map_reduced/config'
 require 'map_reduced/document'
-require 'active_support/core_ext/string/inflections'
+require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'mongo'
 require 'erb'
 require 'pathname'
 
 module MapReduced
-  VERSION = "0.0.6"
+  VERSION = "0.0.7"
 end
